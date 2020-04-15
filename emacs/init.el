b07fd137d7490dc4c98f001d2f30b2d21d3bc91d
hesam <hesamfatemipour@gmail.com>
2020-04-16 02:48:27 +0430

    added some modes and a kitty

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,8 @@
 ; Let the emacs configuration begin!
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-
 
 (setq user-full-name "Hesam"
       user-mail-address "hesaam@riseup.net")
@@ -26,7 +24,7 @@
 ;; installs use-package
 (straight-use-package 'use-package)
 
-;;load theme
+;; load theme
 (use-package doom-themes :straight t :defer t)
 (load-theme 'doom-dracula t)
 
@@ -35,27 +33,84 @@
 (set-face-attribute 'default t :font hesam/font)
 (set-frame-font hesam/font nil t)
 
-;;mode line
-(use-package doom-modeline :straight t :config (doom-modeline-mode 1))
+;;spacemacs mode line
+(use-package spaceline :straight t
+      :config
+      (require 'spaceline-config)
+      (spaceline-spacemacs-theme))
 
-;;vterm configuration
+;; doom mode line 
+(use-package doom-modeline :straight t
+  :disabled t
+    :config
+    (doom-modeline-mode 1))
+
+(use-package nyan-mode :straight t :config (nyan-mode 1))
+
+;; vterm configuration
 (use-package vterm :straight t :bind (("C-c C-t" . vterm)))
 
-;;neotree
+;; neotree
 (use-package neotree :straight t :bind (("<f8>" . neotree-dir)))
 
-;;magit
+;; magit
 (use-package magit :straight t )
 
-;;change cutome file
+;;fzf
+(use-package fzf :straight t :bind (("C-c f" . fzf-directory)))
+
+;;company
+(use-package company :straight t
+  :custom
+  (company-echo-delay 0.1)
+  (company-idle-delay 0.0)
+  (company-minimum-prefix-lenght)
+  (company-tooltip-limit 30)
+  (company-backends '(company-capf company-dabbrev company-files company-dabbrev-code))
+  :bind (:map company-active-map
+              ("C-n" . company-select-next)
+              ("C-p" . company-select-previous)
+              ("C-o" . company-other-backend)
+              ("<tab>" . company-complete-common-or-cycle))
+  :config (global-company-mode t)
+  
+  )
+
+;;language server protocol
+(use-package lsp-mode 
+  :straight t
+  :commands (lsp lsp-deferred)
+  :hook 
+  (prog-mode . lsp)
+  :custom
+  (lsp-auto-guess-root t)
+  :commands (lsp))
+
+
+;; change cutome file
 (use-package cus-edit
   :custom
   (custom-file "~/.emacs.d/temp.el"))
 
-;; basic confs 
+;; improve scrolling
+(use-package emacs
+  :custom
+  ; vertical scrolling
+  (scroll-step 1)
+  (scroll-margin 1)
+  (scroll-conservatively 101)
+  (scroll-up-aggressively 0.01)
+  (scroll-down-aggressively 0.01)
+  (auto-window-vscroll nil)
+  (fast-but-imprecise-scrolling nil)
+  (mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+  (mouse-wheel-progressive-speed t)
+  ;; Horizontal Scroll
+  (hscroll-step 1)
+  (hscroll-margin 1))
+
+;; removes toolbars 
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
 
-;; neotree 
-(global-set-key[f8] 'neotree-dir)
