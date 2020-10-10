3ce2c24fb5ea2c0fafe19c3141f317f85dd0899f
Hesam <hesamfatemipour@gmail.com>
2020-10-10 21:57:26 +0330

    new and improved emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -22,101 +22,47 @@
 (setq user-full-name "Hesam"
       user-mail-address "hesaam@riseup.net")
 
-;; garbage collection
-(setq gc-cons-threshold (* 1024 1024 100)) ;; 100MB for Emacs initialization process
+;; GC
+(setq gc-cons-threshold (* 1024 1024 100))
 (add-hook 'after-init-hook (lambda ()
                              (setq gc-cons-threshold (* 1024 1024 20))))
-;; use staright.el as package manager
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
 
-;; installs use-package
-(straight-use-package 'use-package)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
 
-;; save sessions
-(desktop-save-mode 1)
+;; Theme
+(use-package base16-theme
+  :ensure t
+  :config
+  (load-theme 'darkburn t))
 
-;; load theme
-(use-package doom-themes :straight t :defer t)
-(load-theme 'doom-dracula t)
+;; Font
+(set-face-attribute 'default nil
+                    :family "Ubuntu Mono derivative Powerline"
+                    :height 120
+                    :weight 'normal
+                    :width 'normal)
 
-;; line numbers
-(global-display-line-numbers-mode)
+;; Junk Files
+(use-package cus-edit
+  :custom
+  (custom-file "~/.emacs.d/temp.el"))
 
-;; Window details
-  (setq display-buffer-alist
-        '(("\\*\\(Backtrace\\|Warnings\\|Compile-Log\\|Messages\\)\\*"
-             (display-buffer-in-side-window)
-             (window-width . 0.20)
-             (side . right)
-             (slot . 0))
-          ("^vterm.*"
-            (display-buffer-in-side-window)
-            (window-height . 0.25)
-            (side . bottom)
-	    (slot . 1)
-          )))
-;; font
-;;(defvar hesam/font "Jetbrains Mono-11")
-;;(set-face-attribute 'default t :font hesam/font)
-;;(set-frame-font hesam/font nil t)
+;; Modeline
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+(setq doom-modeline-bar-width 1)
+(setq doom-modeline-project-detection 'project)
+(setq doom-modeline-env-enable-python t)
+(setq doom-modeline-env-enable-go t)
 
-;;Aliases
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; all the icons
-(use-package all-the-icons
-  :disabled t
-  :straight t
-  :commands (all-the-icons-octicon
-	       all-the-icons-faicon
-	       all-the-icons-fileicon
-	       all-the-icons-wicon
-	       all-the-icons-material
-	       all-the-icons-alltheicon))
-
-(use-package all-the-icons-dired
-  :straight t
-  :disabled t
-  :init
-  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
-
-;; flycheck
-(use-package flycheck
-  :straight t
-  :hook (prog-mode . flycheck-mode))
-
-;; spacemacs mode line
-(use-package spaceline :straight t
-      :config
-      (require 'spaceline-config)
-      (spaceline-spacemacs-theme))
-
-;; doom mode line
-(use-package doom-modeline :straight t
-  :disabled t
-    :config
-    (doom-modeline-mode 1))
-
-;; eybrowse
- (use-package eyebrowse :straight t :config (eyebrowse-mode +1))
-
-;; nyan mode(the cat)
-(use-package nyan-mode :straight t :config (nyan-mode 1))
-
-;; vterm configuration
+;; Terminal
 (use-package vterm
-  :straight t
+  :ensure t
   :commands vterm
   :bind (("C-c C-T" . hesam/open-vterm-vertical))
   :config
@@ -131,142 +77,84 @@
     (setq hscroll-margin 0))
   (add-hook 'vterm-mode-hook 'hesam/vterm-hooks))
 
-;; neotree
-(use-package neotree :straight t :bind (("<f8>" . neotree-dir)))
-
-;; magit
-(use-package magit
-  :straight t
-  :commands (magit-status)
-  :bind
-  (("C-x g" . 'magit-status)))
-
-;; fzf
-(use-package fzf :straight t :bind (("C-c f" . fzf-directory)))
-
-;; company
-(use-package company :straight t
-  :custom
-  (company-echo-delay 0.1)
-  (company-idle-delay 0.0)
-  (company-minimum-prefix-lenght)
-  (company-tooltip-limit 30)
-  (company-backends '(company-capf company-dabbrev company-files company-dabbrev-code))
-  :bind (:map company-active-map
-              ("C-n" . company-select-next)
-              ("C-p" . company-select-previous)
-              ("C-o" . company-other-backend)
-              ("<tab>" . company-complete-common-or-cycle))
-  :config (global-company-mode t))
-
-;; general package
-(use-package general :straight t)
-
-;; Helm ftw
-;; Helm
-(use-package helm
-  :ensure t
-  :init
-  (setq helm-mode-fuzzy-match t)
-  (setq helm-completion-in-region-fuzzy-match t)
-  (setq helm-candidate-number-list 50))
-
-;;language server protocol(lsp)
-(use-package lsp-mode
-  :straight t
-  :commands (lsp lsp-deferred)
-  :hook
-  (prog-mode . lsp)
-  :custom
-  (lsp-auto-guess-root t)
-  :commands (lsp))
-
-;; show parentheses
-(show-paren-mode 1)
-(setq show-paren-delay 0)
-
-;;ibuffer
-(use-package ibuffer :bind (("C-x C-b" . 'ibuffer)))
-
-;; ibuffer projectile
-(use-package ibuffer-projectile
-  :straight t
-  :hook (ibuffer . ibuffer-projectile-set-filter-groups))
-
-;; change cutome file
-(use-package cus-edit
-  :custom
-  (custom-file "~/.emacs.d/temp.el"))
-
-;; improve scrolling
-(use-package emacs
-  :custom
-  ; vertical scrolling
-  (scroll-step 1)
-  (scroll-margin 1)
-  (scroll-conservatively 101)
-  (scroll-up-aggressively 0.01)
-  (scroll-down-aggressively 0.01)
-  (auto-window-vscroll nil)
-  (fast-but-imprecise-scrolling nil)
-  (mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-  (mouse-wheel-progressive-speed t)
-  
-  ;; Horizontal Scroll
-  (hscroll-step 1)
-  (hscroll-margin 1))
-
-;; removes toolbars for minimal theme
-(tool-bar-mode 0)
+;; General
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(global-display-line-numbers-mode)
+(setq byte-compile-warnings '(cl-functions))
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
 
-;; confs
-(use-package exec-path-from-shell :straight t
-  :config (exec-path-from-shell-initialize))
+(use-package undo-tree
+  :ensure t
+  :init
+  (global-undo-tree-mode))
 
-(use-package files
-    :config
-    (run-with-idle-timer 1 nil (lambda () (save-buffer) )))
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; language compatability
-
-;; Python
-(use-package python-mode
-  :mode "\\.py\\'"
+(use-package which-key
+  :ensure t
   :config
-  (defun hesam/python-insert-docstring ()
-    (interactive)
-    (insert "'''\n'''")
-    (previous-line))
-  (amirreza/with-backends python-mode (company-capf))
-  :bind
-  (:map python-mode-map
-	("C-c l p d" . hesam/python-insert-docstring)))
+  (which-key-mode))
 
-;; Microsoft lsp
-(use-package lsp-python-ms :straight t)
+;; Auto Complete
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
 
-;; auto pep8
+;; To Search
+(use-package counsel
+  :ensure t
+  )
+
+(use-package swiper
+  :ensure try
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (global-set-key "\C-s" 'swiper)
+    ))
+
+;; FZF
+(use-package fzf
+  :ensure  t
+  :bind (("C-c f" . fzf-directory)))
+
+;; RG
+(use-package rg
+  :ensure t
+  :config
+  (rg-enable-default-bindings))
+;; Python
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode t))
+
+(use-package jedi
+  :ensure t
+  :init
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (add-hook 'python-mode-hook 'jedi:ac-setup))
+
 (use-package py-autopep8
-  :straight t
+  :ensure  t
   :hook python-mode
   :config
   (py-autopep8-enable-on-save))
 
-;; go
-(use-package go-mode
-  :straight t
-  :mode ("\\.go\\'" . go-mode)
-  :init
-  (add-hook 'go-mode-hook (lambda () (add-to-list 'exec-path (concat (getenv "HOME") "/go"))))
-  :config
-  (add-hook 'go-mode-hook (lambda () (interactive)
-                            (add-hook 'before-save-hook 'lsp-format-buffer t t)
-                            (add-hook 'before-save-hook 'lsp-organize-imports t t))))
-
-;; lua mode (requires installing lsp for lua)
-(use-package lua-mode :straight t :mode "\.lua\'")
-
 ;;; init.el ends here
-
